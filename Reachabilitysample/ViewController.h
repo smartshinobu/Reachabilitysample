b6708b2c7f542b3286e31735423968ff400f5a69
smartshinobu <bizan07@bizankomumakku07-no-MacBook-Air-2.local>
2014-11-03 20:07:05 +0900

    Initial Commit

---
+++ Reachabilitysample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Reachabilitysample
+//
+//  Created by ビザンコムマック０７ on 2014/11/03.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
